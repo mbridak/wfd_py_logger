e6c2240d756cb2cff9a1ef73c4126aa85c07e66c
mbridak <michael.bridak@gmail.com>
2022-02-04 13:26:28 -0800

    Added preference class definitions.

---
+++ preferences.py
@@ -0,0 +1,69 @@
+"""Class to store preferences in a json file."""
+import logging
+import os
+from json import dumps, loads
+
+
+class Preferences:
+    """Process preferences"""
+
+    def __init__(self) -> None:
+        """initialize preferences"""
+        self.preference = {
+            "mycallsign": "",
+            "myclass": "",
+            "mysection": "",
+            "power": "0",
+            "usehamdb": 0,
+            "hamdburl": "https://api.hamdb.org",
+            "useqrz": 0,
+            "qrzusername": "",
+            "qrzpassword": "",
+            "qrzurl": "https://xmldata.qrz.com/xml/134",
+            "usehamqth": 0,
+            "hamqthusername": "",
+            "hamqthpassword": "",
+            "hamqthurl": "",
+            "userigctld": 0,
+            "useflrig": 0,
+            "CAT_ip": "localhost",
+            "CAT_port": 12345,
+            "altpower": 0,
+            "outdoors": 0,
+            "notathome": 0,
+            "satellite": 0,
+        }
+
+    def writepreferences(self) -> None:
+        """
+        Write preferences to json file.
+        """
+        try:
+            logging.info("writepreferences:")
+            # home = os.path.expanduser("~")
+            with open(
+                "./wfd_preferences.json", "wt", encoding="utf-8"
+            ) as file_descriptor:
+                file_descriptor.write(dumps(self.preference))
+        except IOError as exception:
+            logging.critical("writepreferences: %s", exception)
+
+    def readpreferences(self) -> None:
+        """
+        Reads preferences from json file.
+        """
+        logging.info("readpreferences:")
+        try:
+            # home = os.path.expanduser("~")
+            if os.path.exists("./wfd_preferences.json"):
+                with open(
+                    "./wfd_preferences.json", "rt", encoding="utf-8"
+                ) as file_descriptor:
+                    self.preference = loads(file_descriptor.read())
+            else:
+                with open(
+                    "./wfd_preferences.json", "wt", encoding="utf-8"
+                ) as file_descriptor:
+                    file_descriptor.write(dumps(self.preference))
+        except IOError as exception:
+            logging.critical("readpreferences: %s", exception)
